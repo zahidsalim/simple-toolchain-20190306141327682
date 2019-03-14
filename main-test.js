76648a5673b12aaa339ecfdee5f434a39b630ce7
HCLTECH\zahid.s <zahid.s@hcl.com>
2019-03-14 18:55:17 +0530

    Committed

--- main-test.js
+++ main-test.js
@@ -8,13 +8,13 @@ function importTest(name, path) {
 }
 
 
-describe("top", function () {
+describe("Executing Main Test Script", function () {
     beforeEach(function () {
        console.log("running something before each test");
     });
     importTest("a", './web-tests/instant-sauce-mocha-test2.js');
     importTest("b", './web-tests/instant-sauce-mocha-test3.js');
-    
+
   //  importTest("c", './web-tests/instant-sauce-mocha-test1.js');
   //  importTest("d", './web-tests/instant-sauce-mocha-test4.js');
     after(function () {
