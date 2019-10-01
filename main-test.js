d04b72d746cbfd0bf02789077a0d8a28e6f17cf7
HCLTECH\zahid.s <zahid.s@hcl.com>
2019-10-01 13:47:18 +0530

    Added for SauceLab

--- main-test.js
+++ main-test.js
@@ -13,7 +13,7 @@ describe("Executing Main Test Script", function () {
     beforeEach(function () {
        console.log("running something before each test");
     });
-    importTest("a", './web-tests/instant-sauce-mocha-test2.js');
+  //  importTest("a", './web-tests/instant-sauce-mocha-test2.js');
     importTest("b", './web-tests/instant-sauce-mocha-test3.js');
 
   //  importTest("c", './web-tests/instant-sauce-mocha-test1.js');
