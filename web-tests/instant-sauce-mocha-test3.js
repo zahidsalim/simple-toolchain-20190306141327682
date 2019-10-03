be5258f14b90ce07f1e20a17aa25051705bb9b25
HCLTECH\zahid.s <zahid.s@hcl.com>
2019-10-03 16:06:22 +0530

    Added for SauceLab

--- web-tests/instant-sauce-mocha-test3.js
+++ web-tests/instant-sauce-mocha-test3.js
@@ -1,82 +1,53 @@
+const promise = require('selenium-webdriver');
+let expect = require('chai').expect;
+let webdriver = require('selenium-webdriver');
 
-var webdriver = require('selenium-webdriver'),
-    test = require('selenium-webdriver/testing'),
-    assert = require('assert'),
-  //  username = process.env.SAUCE_USERNAME,
-      username = "yadav.o";
-//    accessKey = process.env.SAUCE_ACCESS_KEY,
-      accessKey = "bb24ec0b-cf6f-4c84-acbe-5fe1a6fbc6b5",
-      saucelabs = new SauceLabs({
-      username: username,
-      password: accessKey
-    }),
-    /* Change the baseURL to your application URL */
-    baseUrl = "https://sample-cloud-native-toolchain-slabs.mybluemix.net/";
-    var driver;
+/* promise manager for selenium-webdriver is going to be deprecated */
+/* you must use async/await as an alternative */
+promise.USE_PROMISE_MANAGER = false;
 
-    function importTest(name, path) {
-    console.log('Entered importTest. Path=',path);
-    describe(name, function () {
-        require(path);
-        });
-    }
+let username = "yadav.o",
+    accessKey = p"bb24ec0b-cf6f-4c84-acbe-5fe1a6fbc6b5",
+    appURL = "https://sample-cloud-native-toolchain-slabs.mybluemix.net/",
+    tags = ["sauceDemo", "async", "node", "webdriverjs", "headless" ],
+    driver;
 
-
-describe('Instant Sauce Test Module 3', function() {
-    this.timeout(40000);
-    /* Now we will add a beforeEach method using the Mocha framework in order to
-    set prerequiste tasks for each test case, in this case we're setting the driver capabilities.
-     */
-    beforeEach(function (done) {
-        var testName = this.currentTest.title;
-        console.log('TEST NAME is ==> ',testName);
-        driver = new webdriver.Builder().withCapabilities({
-            'browserName': 'chrome',
-            'platform': 'Windows 10',
-            'version': '59.0',
-            'username': username,
-            'accessKey': accessKey,
-            'build': 'Onboarding Sample App - NodeJS',
-            'name': '3-cross-browser',
-        }).usingServer("http://" + username + ":" + accessKey +
-            "@ondemand.saucelabs.com:80/wd/hub").build();
-
-        driver.getSession().then(function (sessionid) {
+describe ('headless firefox test', function() {
+    this.timeout(50000);
+    beforeEach(async function () {
+        driver = await new webdriver.Builder().withCapabilities({
+            'browserName': 'firefox',
+            'platformName': 'linux',
+            'browserVersion': 'latest',
+            'sauce:options': {
+                'username': username,
+                'accessKey': accessKey,
+                'seleniumVersion': '3.141.59',
+                'build': 'Sample Headless Tests',
+                'name': 'headless-firefox-test-js',
+                'maxDuration': 3600,
+                'idleTimeout': 1000,
+                'tags': tags
+            }}).usingServer("https://ondemand.us-east-1.saucelabs.com/wd/hub")
+            .build();
+        await driver.getSession().then(function (sessionid) {
             driver.sessionID = sessionid.id_;
         });
-
-        done();
     });
 
-    /* Here we add any post-requisite tasks, such as sending the test results to Sauce Labs.com*/
-    afterEach(function (done) {
-      console.log('afterEach 3...');
-        driver.executeScript("sauce:job-result=" + (true ? "passed" : "failed"));
-        driver.quit();
-        saucelabs.updateJob(driver.sessionID, {
-          name: title,
-          passed: passed
-        }, done);
-
+    afterEach(async function() {
+        await driver.executeScript("sauce:job-result=" + (this.currentTest.state));
+        await driver.quit();
     });
 
-    //importTest("a", './web-tests/instant-sauce-mocha-test2.js');
-
-    after(function () {
+	after(function () {
         console.log("after all tests");
     });
 
-    it('should-open-chrome ', function (done) {
-        driver.get(baseUrl);
-        driver.getTitle().then(function (title) {
-            console.log("Title is: " + title);
-            assert(true);
-            done();
-        });
+    it('get-title-test', async function() {
+        await driver.get(appURL);
+        const title = await driver.getTitle();
+        console.log('Page Title is: ' + title);
+        expect(title).equals('Swag Labs');
     });
-
-
-
-
-
 });
